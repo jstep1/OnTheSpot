d49be253f8b670841e4f89122c0bebf5a4b39e37
Jeremy Stepanovich <jeremystepanovich@Jeremys-MacBook-Pro.local>
2018-02-12 22:32:13 -0600

    backup initial files for project

---
+++ app.js
@@ -0,0 +1,11 @@
+var favArtists = function() {
+    $("#artists").empty();
+    var queryURL = "https://api.spotify.com/v1/artists/" + artistID;
+    
+}
+
+var addArtist = function () {
+
+}
+
+$(document).on("click", ".btn", addArtist);
